b80fce00ef8cf64bca6b55f465ed500a5895f3c1
jairoBerdugo <jairoberdugo@hotmail.es>
2023-10-18 11:17:13 -0500

    update next version

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -3,7 +3,6 @@ import React, { useState } from "react";
 import GithubIcon from "../../../public/github-icon.svg";
 import LinkedinIcon from "../../../public/linkedin-icon.svg";
 import BehanceIcon from "../../../public/behance-icon.svg";
-
 import Link from "next/link";
 import Image from "next/image";
 
