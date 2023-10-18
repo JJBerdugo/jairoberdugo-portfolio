b0c4d548a6f0f2308991e50093be7d04cfc80b49
jairoBerdugo <jairoberdugo@hotmail.es>
2023-10-18 03:04:14 -0500

    test to vercel

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -49,7 +49,7 @@ const EmailSection = () => {
             <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-yellow-600 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
             <div className="z-10">
                 <h5 className="text-xl font-bold text-white my-2">
-                    ' JairoBerdu.go [./portFolio ] '
+                    JairoBerdu.go [./portFolio ]
                 </h5>
                 <p className="text-[#ADB7BE] mb-4 max-w-md">
                     {" "}
